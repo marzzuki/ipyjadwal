06a4bdd4e90a9c79004fb8d87f5b5d63f72b49b8
Mustafa Marzouk <40539669+marzzuki@users.noreply.github.com>
2025-12-27 00:24:12 +0200

    Update setup.py version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="ipyjadwal",
-    version="0.1.3",
+    version="0.1.4",
     python_requires=">=3.7",
     description="A clean, interactive Google Sheet explorer for Colab (Jadwal)",
     long_description=open("README.md").read(),
